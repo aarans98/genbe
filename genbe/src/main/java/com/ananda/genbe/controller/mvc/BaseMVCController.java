47197fbd8f7d83c7cd5b1154c2030856f296244f
Ananda <Ananda@DESKTOP-476N35L>
2020-09-08 14:43:48 +0700

    Penambahan HTML dengan menggunakan DataTable

--- genbe/src/main/java/com/ananda/genbe/controller/mvc/BaseMVCController.java
+++ genbe/src/main/java/com/ananda/genbe/controller/mvc/BaseMVCController.java
@@ -2,16 +2,16 @@ package com.ananda.genbe.controller.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class BiodataController {
+public class BaseMVCController {
 	
 	@GetMapping("base")
 	public String base() {
 		return "base";
 	}
 	
+	// Menggunakan simple table
 	@GetMapping("/soal1")
 	public String soal1() {
 		return "/biodata/soal1";
@@ -25,5 +25,21 @@ public class BiodataController {
 	@GetMapping("/soal3")
 	public String soal3() {
 		return "/biodata/soal3";
+	}
+	
+	// Menggunakan data table
+	@GetMapping("/1soal")
+	public String soalSatu() {
+		return "/biodata/1soal";
+	}
+	
+	@GetMapping("/2soal")
+	public String soalDua() {
+		return "/biodata/2soal";
+	}
+	
+	@GetMapping("/3soal")
+	public String soalTiga() {
+		return "/biodata/3soal";
 	}
 }
